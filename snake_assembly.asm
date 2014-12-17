ad77f046ee1af830945d44b950f4c3da0dfb2089
Gearlles <gearlles@gmail.com>
2014-12-17 19:31:25 -0300

    mudando de ordem as rotinas

--- snake_assembly.asm
+++ snake_assembly.asm
@@ -1,8 +1,8 @@
 $include(REG52.inc)
 $include(Random.asm)
+$include(LCD.asm)
 
-
-SNAKE_MAX_SIZE SET 0x09
+SNAKE_MAX_SIZE SET 0x02
 SNAKE_MAX_SIZE_ADDRESS SET 0x50
 
 SNAKE_SCREEN_WIDTH SET 0x54
@@ -105,114 +105,63 @@ SNAKE_INIT:
     MOV R0, #SNAKE_ADD_Y_ADDRESS
     MOV @R0, #01H
     RET
+
+code
+SNAKE_UPDATE:
+    LCALL SNAKE_CHECK_GAME_END
+    JNC SNAKE_UPDATE_START
+    LCALL SNAKE_MAIN
     
-code
-NTMJ_EXTRACT_LCD_COLUMN:
-    PUSH ACC
-    MOV A, DPH
-    PUSH ACC
-    MOV A, DPL
-    PUSH ACC
-    MOV R5, #0
-    MOV R3, #8 ; Contador de bits buscados
-    NTMJ_EXTRACT_LCD_LOOP: 
-        MOVX A, @DPTR
-        MOV R4, A ; Salva o pixel em R4
-        MOV A, R5
-        RL A ; Rotaciona o valor anterior
-        ORL A, R4 ; Adiciona o pixel lido agora
-        MOV R5, A ; Sobrescreve o valor anterior
-        MOV B, #84 ; Vai buscar o proximo
-        LCALL NTMJ_INC_DPTR
-        DJNZ R3 NTMJ_EXTRACT_LCD_LOOP
-    MOV B, R5
-    POP ACC
-    MOV DPL, A
-    POP ACC
-    MOV DPH, A
-    POP ACC
-    RET
- 
-code
-NTMJ_DRAW_TO_LCD:
-    PUSH PSW ; Salva a pagina atual
-    PUSH ACC ; Salva o ACC atual
-    SETB RS1 ; Vai para a pagina do LCD
-    SETB RS0 ; Vai para a pagina do LCD
-    LCALL LCD_CLEAR
-    MOV DPTR, #000h 
-    MOV R6, #6 ; Laco externo eh y
-    MOV lcd_Y, #0 ; Comeca em 00
-    NTMJ_DRAW_LCD_LINE:
-        MOV lcd_X, #0 ; Comeca em 00
-        MOV R7, #84 ; Laco interno eh x
-        NTMJ_DRAW_LCD_COLUMN:
-            LCALL NTMJ_EXTRACT_LCD_COLUMN
-            MOV lcd_bus, B
-            MOV A, lcd_X
-            MOV B, lcd_Y
-            LCALL LCD_ACC_XY
-            MOV B, lcd_bus
-            LCALL LCD_ACC_DRAW
-            INC lcd_X
-            MOV B, #1
-            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
-            ; Diminui o contador de colunas e repete ate q complete
-            DJNZ R7 NTMJ_DRAW_LCD_COLUMN
-            INC lcd_Y
-            MOV B, #252
-            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
-            MOV B, #252
-            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
-            MOV B, #84
-            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
-            ; Diminui o contador de linhas e repete ate q complete
-            DJNZ R6 NTMJ_DRAW_LCD_LINE
- 
-    POP ACC ; Restaura o ACC anterior
-    POP PSW ; Restaura a pagina anterior
+    SNAKE_UPDATE_START:
+        MOV R0, #SNAKE_SIZE_ADDRESS
+        MOV A, @R0
+        MOV R3, A
+        LOOP_UPDATE_BODY:
+            MOV A, R3
+            CJNE A, #001h, BODY
+            SETB C
+        BODY:
+            JC AFTER_LOOP
+            
+            MOV    A, R3
+            ADD    A, #SNAKE_X_ARRAY_START_ADDRESS + 0FFH
+            MOV    R0, A
+            MOV    A, R3
+            ADD    A, #SNAKE_X_ARRAY_START_ADDRESS
+            MOV    R1, A
+            MOV    A, @R0
+            MOV    @R1, A
+            
+            MOV    A, R3
+            ADD    A, #SNAKE_Y_ARRAY_START_ADDRESS + 0FFH
+            MOV    R0, A
+            MOV    A, R3
+            ADD    A, #SNAKE_Y_ARRAY_START_ADDRESS
+            MOV    R1, A
+            MOV    A, @R0
+            MOV    @R1, A
+            
+            DEC R3
+            SJMP LOOP_UPDATE_BODY
+        AFTER_LOOP:
+           MOV R0, #SNAKE_ADD_X_ADDRESS
+           MOV A, @R0
+           MOV R0, #SNAKE_X_ARRAY_START_ADDRESS + 02H
+           ADD A, @R0
+           MOV R0, #SNAKE_X_ARRAY_START_ADDRESS + 01H
+           MOV @R0, A
+           
+           MOV R0, #SNAKE_ADD_Y_ADDRESS
+           MOV A, @R0
+           MOV R0, #SNAKE_Y_ARRAY_START_ADDRESS + 02H
+           ADD A, @R0
+           MOV R0, #SNAKE_Y_ARRAY_START_ADDRESS + 01H
+           MOV @R0, A
     RET
     
-code
-NTMJ_INC_DPTR:
-    PUSH ACC
-    MOV A, DPL ; Pega o DPL pra ACC
-    ADD A, B ; Soma B (pula um byte)
-    MOV DPL, A ; Salva no DPL o ACC
-    MOV A, DPH ; Pega o DPH pro ACC
-    ADDC A, #00h ; Adiciona o carry da operacao anterior
-    MOV DPH, A ; Salva no DPH o ACC
-    POP ACC
-    RET
-
-
-code
-LCD_ACC_XY:
-    PUSH ACC 
-    PUSH PSW 
-    SETB RS1
-    CLR RS0
-    MOV lcd_X, A
-    MOV lcd_Y, B
-    LCALL LCD_XY
-    POP PSW
-    POP ACC
-    ret
-	
-code
-LCD_ACC_DRAW:
-    PUSH ACC 
-    PUSH PSW 
-    SETB RS1
-    CLR RS0
-    MOV lcd_bus, B
-    LCALL LCD_DRAW
-    POP PSW
-    POP ACC
-    ret
-
 code
 SNAKE_CHECK_GAME_END:
+    CLR C
     CLR k
     MOV A, SNAKE_SIZE_ADDRESS
     CJNE A, SNAKE_MAX_SIZE, SNAKE_ELSE_MAX
@@ -261,58 +210,7 @@ SNAKE_CHECK_GAME_END:
           RLC A
           MOV R6, #000H
           MOV R7, A
-    RET
-
-code
-SNAKE_UPDATE:
-    LCALL SNAKE_CHECK_GAME_END
-    JC SNAKE_MAIN
-        
-    MOV R0, #SNAKE_SIZE_ADDRESS
-    MOV A, @R0
-    MOV R3, A
-    LOOP_UPDATE_BODY:
-        MOV A, R3
-        CJNE A, #001h, BODY
-        SETB C
-    BODY:
-        JC AFTER_LOOP
-        
-        MOV    A, R3
-        ADD    A, #SNAKE_X_ARRAY_START_ADDRESS + 0FFH
-        MOV    R0, A
-        MOV    A, R3
-        ADD    A, #SNAKE_X_ARRAY_START_ADDRESS
-        MOV    R1, A
-        MOV    A, @R0
-        MOV    @R1, A
-        
-        MOV    A, R3
-        ADD    A, #SNAKE_Y_ARRAY_START_ADDRESS + 0FFH
-        MOV    R0, A
-        MOV    A, R3
-        ADD    A, #SNAKE_Y_ARRAY_START_ADDRESS
-        MOV    R1, A
-        MOV    A, @R0
-        MOV    @R1, A
-        
-        DEC R3
-        SJMP LOOP_UPDATE_BODY
-    AFTER_LOOP:
-       MOV R0, #SNAKE_ADD_X_ADDRESS
-       MOV A, @R0
-       MOV R0, #SNAKE_X_ARRAY_START_ADDRESS + 02H
-       ADD A, @R0
-       MOV R0, #SNAKE_X_ARRAY_START_ADDRESS + 01H
-       MOV @R0, A
-       
-       MOV R0, #SNAKE_ADD_Y_ADDRESS
-       MOV A, @R0
-       MOV R0, #SNAKE_Y_ARRAY_START_ADDRESS + 02H
-       ADD A, @R0
-       MOV R0, #SNAKE_Y_ARRAY_START_ADDRESS + 01H
-       MOV @R0, A
-    RET
+    RET    
     
 code
 SNAKE_READ_BUTTONS:
@@ -537,4 +435,109 @@ SNAKE_CONVERT_MEMORY:
               LJMP   SNAKE_CONVERT_MEMORY_LOOP
         SNAKE_CONVERT_MEMORY_LOOP_END:
         RET     
+
+code
+NTMJ_EXTRACT_LCD_COLUMN:
+    PUSH ACC
+    MOV A, DPH
+    PUSH ACC
+    MOV A, DPL
+    PUSH ACC
+    MOV R5, #0
+    MOV R3, #8 ; Contador de bits buscados
+    NTMJ_EXTRACT_LCD_LOOP: 
+        MOVX A, @DPTR
+        MOV R4, A ; Salva o pixel em R4
+        MOV A, R5
+        RL A ; Rotaciona o valor anterior
+        ORL A, R4 ; Adiciona o pixel lido agora
+        MOV R5, A ; Sobrescreve o valor anterior
+        MOV B, #84 ; Vai buscar o proximo
+        LCALL NTMJ_INC_DPTR
+        DJNZ R3 NTMJ_EXTRACT_LCD_LOOP
+    MOV B, R5
+    POP ACC
+    MOV DPL, A
+    POP ACC
+    MOV DPH, A
+    POP ACC
+    RET
+ 
+code
+NTMJ_DRAW_TO_LCD:
+    PUSH PSW ; Salva a pagina atual
+    PUSH ACC ; Salva o ACC atual
+    SETB RS1 ; Vai para a pagina do LCD
+    SETB RS0 ; Vai para a pagina do LCD
+    LCALL LCD_CLEAR
+    MOV DPTR, #000h 
+    MOV R6, #6 ; Laco externo eh y
+    MOV lcd_Y, #0 ; Comeca em 00
+    NTMJ_DRAW_LCD_LINE:
+        MOV lcd_X, #0 ; Comeca em 00
+        MOV R7, #84 ; Laco interno eh x
+        NTMJ_DRAW_LCD_COLUMN:
+            LCALL NTMJ_EXTRACT_LCD_COLUMN
+            MOV lcd_bus, B
+            MOV A, lcd_X
+            MOV B, lcd_Y
+            LCALL LCD_ACC_XY
+            MOV B, lcd_bus
+            LCALL LCD_ACC_DRAW
+            INC lcd_X
+            MOV B, #1
+            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
+            ; Diminui o contador de colunas e repete ate q complete
+            DJNZ R7 NTMJ_DRAW_LCD_COLUMN
+            INC lcd_Y
+            MOV B, #252
+            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
+            MOV B, #252
+            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
+            MOV B, #84
+            LCALL NTMJ_INC_DPTR ; Vai para a proxima coluna
+            ; Diminui o contador de linhas e repete ate q complete
+            DJNZ R6 NTMJ_DRAW_LCD_LINE
+ 
+    POP ACC ; Restaura o ACC anterior
+    POP PSW ; Restaura a pagina anterior
+    RET
+    
+code
+NTMJ_INC_DPTR:
+    PUSH ACC
+    MOV A, DPL ; Pega o DPL pra ACC
+    ADD A, B ; Soma B (pula um byte)
+    MOV DPL, A ; Salva no DPL o ACC
+    MOV A, DPH ; Pega o DPH pro ACC
+    ADDC A, #00h ; Adiciona o carry da operacao anterior
+    MOV DPH, A ; Salva no DPH o ACC
+    POP ACC
+    RET
+
+
+code
+LCD_ACC_XY:
+    PUSH ACC 
+    PUSH PSW 
+    SETB RS1
+    CLR RS0
+    MOV lcd_X, A
+    MOV lcd_Y, B
+    LCALL LCD_XY
+    POP PSW
+    POP ACC
+    ret
+	
+code
+LCD_ACC_DRAW:
+    PUSH ACC 
+    PUSH PSW 
+    SETB RS1
+    CLR RS0
+    MOV lcd_bus, B
+    LCALL LCD_DRAW
+    POP PSW
+    POP ACC
+    ret
 END
